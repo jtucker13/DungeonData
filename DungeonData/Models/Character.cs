d3e6cf2999088c3449edb46060a948d6b587e1d5
jtucker <jtucker@cs.odu.edu>
2021-02-28 22:54:40 -0500

    Add project files.

---
+++ DungeonData/Models/Character.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DungeonData.Models
+{
+    public enum CharClass
+    { 
+        Barbarian,
+        Bard,
+        Cleric,
+        Druid,
+        Fighter,
+        Monk,
+        Paladin,
+        Ranger,
+        Rogue,
+        Sorcerer,
+        Warlock,
+        Wizard
+    }
+    public enum CharSubclass
+    {
+
+    }
+    public enum Race
+    {
+        LightfootHalfling,
+        Halfling,
+        Human,
+        Elf,
+        WoodElf,
+        HalfElf,
+        HighElf,
+        DarkElf,
+        HalfOrc,
+        Gnome,
+        RockGnome,
+        Dwarf,
+        HillDwarf,
+        Dragonborn,
+        Tiefling
+    }
+    public enum Status
+    {
+        Healthy,
+        Blinded,
+        Charmed,
+        Deafened,
+        Frightened,
+        Grappled,
+        Incapacitated,
+        Invisible,
+        Paralyzed,
+        Petrified,
+        Poisoned,
+        Prone,
+        Restrained,
+        Stunned,
+        Unconscious
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum Alignment
+    {
+        CEvil,
+        CNeutral,
+        CGood,
+        TEvil,
+        TNeutral,
+        TGood,
+        LEvil,
+        LNeutral,
+        LGood
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Character
+    {
+        public int CharacterID { get; set; }
+        public string Name { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
+        public int Exhaustion { get; set; }
+        public int CurrentHP { get; set; }
+        public int MaxHP { get; set; }
+        public int Speed { get; set; }
+    }
+}
